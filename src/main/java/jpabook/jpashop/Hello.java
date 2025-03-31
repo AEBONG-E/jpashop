cba1c813f9a6fb0ee8ec11ebe088f807f10fe316
hooney(remote) <108711069+AEBONG-E@users.noreply.github.com>
2025-03-31 10:02:46 +0900

    #2 - Feat: Setting up the view (creating test controller and template file)

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hello {
+
+    public String data;
+
+}
